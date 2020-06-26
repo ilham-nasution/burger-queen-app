40a5cd19d7cf862e26143b3f2627e57252c87c05
Ilham Rahman <ilhaamrahman@gmail.com>
2020-06-26 10:45:19 +0700

    initialize CRA and Bootstrap

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">Burger Queen</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+        <Nav>
+          <Nav.Link href="#home">Builder</Nav.Link>
+          <Nav.Link href="#link">Orders</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
