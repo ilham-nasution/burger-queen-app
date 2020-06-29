8d66056ac279cfd095c5289c6bfc0ad7d80dce85
Ilham Rahman <ilhaamrahman@gmail.com>
2020-06-29 13:56:38 +0700

    add burger menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import "./style.scss";
 import Burger from "./components/Burger";
 import { Container, Col, Row } from "react-bootstrap";
+import Menu from "./components/Menu";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Container>
-        <Row>
-          <Col>
-            <Burger />
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { meat: 0, cheese: 0 };
+  }
+
+  handleMeatPlusBtn = () => {
+    this.setState({
+      meat: ++this.state.meat,
+    });
+  };
+
+  handleMeatMinusBtn = () => {
+    this.setState({
+      meat: Math.max(0, --this.state.meat),
+    });
+  };
+
+  handleCheesePlusBtn = () => {
+    this.setState({
+      cheese: ++this.state.cheese,
+    });
+  };
+
+  handleCheeseMinusBtn = () => {
+    this.setState({
+      cheese: Math.max(0, --this.state.cheese),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Container>
+          <Row>
+            <Col>
+              <Burger meat={this.state.meat} cheese={this.state.cheese} />
+              <Menu
+                meat={this.state.meat}
+                cheese={this.state.cheese}
+                cheeseMinusBtn={this.handleCheeseMinusBtn}
+                meatMinusBtn={this.handleMeatMinusBtn}
+                cheesePlusBtn={this.handleCheesePlusBtn}
+                meatPlusBtn={this.handleMeatPlusBtn}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
+const Menu = (props) => {
+  return (
+    <Form>
+      <Form.Group as={Row}>
+        <Form.Label column sm="2">
+          Meat
+        </Form.Label>
+        <Col xs={7}>
+          <Form.Control type="text" value={props.meat} />
+        </Col>
+        <Col>
+          <Button onClick={props.meatPlusBtn} variant="outline-primary">
+            +
+          </Button>
+        </Col>
+        <Col>
+          <Button onClick={props.meatMinusBtn} variant="outline-primary">
+            -
+          </Button>
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row}>
+        <Form.Label column sm="2">
+          Cheese
+        </Form.Label>
+        <Col xs={7}>
+          <Form.Control type="text" value={props.cheese} />
+        </Col>
+        <Col>
+          <Button onClick={props.cheesePlusBtn} variant="outline-primary">
+            +
+          </Button>
+        </Col>
+        <Col>
+          <Button onClick={props.cheeseMinusBtn} variant="outline-primary">
+            -
+          </Button>
+        </Col>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default Menu;
