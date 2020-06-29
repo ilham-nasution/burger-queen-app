018f748b97d6c0970a931008e1b7e0f1d586b3b6
Ilham Rahman <ilhaamrahman@gmail.com>
2020-06-29 14:54:29 +0700

    add animation

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { slideInRight } from "react-animations";
+import Radium, { StyleRoot } from "radium";
 import "./Burger.css";
+
+const styles = {
+  slideInRight: {
+    animation: "x 1s",
+    animationName: Radium.keyframes(slideInRight, "slideInRight"),
+  },
+};
 
 const Burger = (props) => {
   return (
-    <div className="Burger">
-      <div className="BreadTop"></div>
-      {[...Array(props.cheese)].map((_, i) => (
-        <div className="Cheese"></div>
-      ))}
-      {[...Array(props.meat)].map((_, i) => (
-        <div className="Meat"></div>
-      ))}
-      <div className="BreadBottom"></div>
-    </div>
+    <StyleRoot>
+      <div className="Burger">
+        <div className="BreadTop"></div>
+        {[...Array(props.cheese)].map((_, i) => (
+          <div style={styles.slideInRight} className="Cheese"></div>
+        ))}
+        {[...Array(props.meat)].map((_, i) => (
+          <div style={styles.slideInRight} className="Meat"></div>
+        ))}
+        <div className="BreadBottom"></div>
+      </div>
+    </StyleRoot>
   );
 };
 
