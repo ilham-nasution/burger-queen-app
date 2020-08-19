0685375f2dc32ff0e4b30e4f276868eaf90f2380
Ilham Rahman <ilhaamrahman@gmail.com>
2020-08-19 13:30:09 +0700

    add animation

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "./Burger.css";
 
 const Burger = ({ meat, cheese }) => {
   return (
     <div className="Burger">
       <div className="BreadTop"></div>
-      {[...Array(cheese)].map((_, i) => (
-        <div key={i} className="Cheese"></div>
-      ))}
-      {[...Array(meat)].map((_, i) => (
-        <div key={i} className="Meat"></div>
-      ))}
+      <TransitionGroup>
+        {[...Array(cheese)].map((_, i) => (
+          <CSSTransition key={i} timeout={500} classNames="animation">
+            <div className="Cheese"></div>
+          </CSSTransition>
+        ))}
+        {[...Array(meat)].map((_, i) => (
+          <CSSTransition key={i} timeout={500} classNames="animation">
+            <div key={i} className="Meat"></div>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
       <div className="BreadBottom"></div>
     </div>
   );
